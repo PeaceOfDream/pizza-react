3ab4aee6b7a1b55b79a69036320be8f49739f61e
Dima Bart <3088742@mail.ru>
2022-06-23 23:06:30 +0300

    add pagination

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,25 +4,30 @@ import { Sort } from '../components/Sort';
 import { PizzaBlock } from '../components/PizzaBlock';
 import { useEffect, useState } from 'react';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import { Pagination } from '../Pagination';
 
-export const Home = () => {
+export const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const [sortType, setSortType] = useState({
-	name: 'популярности',
-	sortProperty: 'rating'
+    name: 'популярности',
+    sortProperty: 'rating',
   });
 
   useEffect(() => {
-        setIsLoading(true);
+    setIsLoading(true);
 
-		  const sortBy = sortType.sortProperty.replace('-', '')
-		  const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
-		  const category = categoryId > 0 ? `category=${categoryId}` : '';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `&category=${categoryId}` : '';
+    const search = searchValue ? `&search=${searchValue}` : '';
 
-    fetch(`https://62b20abe20cad3685c886056.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(
+      `https://62b20abe20cad3685c886056.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`,
+    )
       .then((res) => {
         return res.json();
       })
@@ -32,28 +37,23 @@ export const Home = () => {
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((item) => <PizzaBlock key={item.id} {...item} />);
+
+  const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories 
-		  	value={categoryId} 
-			onChangeCategory={(id) => setCategoryId(id)} 
-			/>
-        <Sort 
-		  	value={sortType}
-			onChangeSort={(id) => setSortType(id)}
-		  />
+        <Categories value={categoryId} onChangeCategory={(id) => setCategoryId(id)} />
+        <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-          : items.map((item) => <PizzaBlock key={item.id} {...item} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+		<Pagination onChangePage={number => setCurrentPage(number)}/>
     </div>
   );
 };
 
-// export default Home;
+
